a0fa836e8aed7071ee2f71060a40d26b6c44dd2e
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-12-04 03:03:28 +0000

    fix: use jiti

--- src/index.js
+++ src/index.js
@@ -3,21 +3,25 @@ const { Router } = require('express');
 const mountMiddlewares = require('./mountMiddlewares');
 const mountRoutes = require('./mountRoutes');
 
-module.exports = async function(
+module.exports = function(
   root,
-  { cwd = process.cwd(), viewExtensions = [] } = {}
+  { cwd = process.cwd(), viewExtensions = [], jitiOptions } = {}
 ) {
   root = path.resolve(cwd, root);
   // The base router that'll hold everything
   const router = Router();
 
   // First mount the middlewares
-  const routesFromMiddlewares = await mountMiddlewares(router, root, { cwd });
+  const routesFromMiddlewares = mountMiddlewares(router, root, {
+    cwd,
+    jitiOptions
+  });
 
   // And then the actual routes
-  await mountRoutes(router, root, {
+  mountRoutes(router, root, {
     routes: routesFromMiddlewares,
-    viewExtensions
+    viewExtensions,
+    jitiOptions
   });
 
   return router;
